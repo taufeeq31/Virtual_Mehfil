a1b9cfab23787e11ebca4b75167227b5478c2bdb
Mohd Taufiq <taufiq31878@gmail.com>
2025-10-07 17:20:51 +0530

    db connected and env.js completed

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,14 @@
 import express from 'express';
 import { ENV } from './config/env.js';
+import { connectDB } from './config/db.js';
 
 const app = express();
-
 
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
 
-app.listen(ENV.PORT , () => console.log('Server is running on port:',ENV.PORT));
+app.listen(ENV.PORT, () => {
+    console.log('Server is running on port:', ENV.PORT);
+    connectDB();
+});
