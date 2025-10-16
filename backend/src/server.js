915bd9e73c7646865c386160e523537302ffb510
Mohd Taufiq <taufiq31878@gmail.com>
2025-10-16 18:08:46 +0530

    Recovered backend/src/server.js from auth branch

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,36 @@
 import express from 'express';
 import { ENV } from './config/env.js';
+import { connectDB } from './config/db.js';
+import { clerkMiddleware } from '@clerk/express';
+import { functions, inngest } from './config/inngest.js';
+import { serve } from 'inngest/express';
 
 const app = express();
 
+app.use(express.json());
+app.use(clerkMiddleware());
+
+app.use('/api/inngest', serve({ client: inngest, functions }));
 
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
 
-app.listen(ENV.PORT , () => console.log('Server is running on port:',ENV.PORT));
+const startServer = async () => {
+    try {
+        await connectDB();
+        if (ENV.NODE_ENV !== 'production') {
+            app.listen(ENV.PORT, () => {
+                console.log('Server is running on port:', ENV.PORT);
+                connectDB();
+            });
+        }
+    } catch (error) {
+        console.error('Error starting server:', error);
+        process.exit(1);
+    }
+};
+
+startServer();
+
+export default app;
