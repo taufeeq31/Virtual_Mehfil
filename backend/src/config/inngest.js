cc57b4a54755bc6ef8a845e0d32574635b87dfa8
Mohd Taufiq <taufiq31878@gmail.com>
2025-10-19 12:56:45 +0530

    tinyy fix

--- backend/src/config/inngest.js
+++ backend/src/config/inngest.js
@@ -11,7 +11,7 @@ const syncUser = inngest.createFunction(
     { event: 'clerk/user.created' },
     async ({ event }) => {
         await connectDB();
-        const { email, name, first_name, last_name, id } = event.data;
+        const { id, email_addresses, first_name, last_name, image_url } = event.data;
 
         const newUser = {
             clerkId: id,
